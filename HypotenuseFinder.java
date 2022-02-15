c150f9b0afb08bc8bca59e49abca8a4f5fc3f3b7
N3sB <enesbaysl@outlook.com>
2022-02-15 22:58:03 +0300

    Hypotenuse Finder

---
+++ HypotenuseFinder.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HypotenuseFinder {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        double firstSide, secondSide,hypotenuse;
+
+        System.out.print("Please enter the first side of triangle: ");
+        firstSide = scanner.nextDouble();
+
+        System.out.print("Please enter the second side of triangle: ");
+        secondSide = scanner.nextDouble();
+        scanner.close();
+
+        hypotenuse = Math.sqrt((firstSide*firstSide)+(secondSide*secondSide));
+
+        System.out.println("Triangle's hypotenuse is " + hypotenuse);
+
+
+    }
+}
