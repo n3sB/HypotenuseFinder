2edcaf9aa5f093159ad60ce0241d439a861a70ea
N3sB <enesbaysl@outlook.com>
2022-02-19 14:02:56 +0300

    Update HypotenuseFinder.java

--- HypotenuseFinder.java
+++ HypotenuseFinder.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class HypotenuseFinder {
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
 
-        double firstSide, secondSide,hypotenuse;
+        double firstSide, secondSide, hypotenuse;
 
         System.out.print("Please enter the first side of triangle: ");
         firstSide = scanner.nextDouble();
@@ -14,10 +14,9 @@ public class HypotenuseFinder {
         secondSide = scanner.nextDouble();
         scanner.close();
 
-        hypotenuse = Math.sqrt((firstSide*firstSide)+(secondSide*secondSide));
+        hypotenuse = Math.sqrt((firstSide * firstSide) + (secondSide * secondSide));
 
         System.out.println("Triangle's hypotenuse is " + hypotenuse);
-
 
     }
 }
